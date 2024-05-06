cc6ce1fb9067f0cf9d93fee6be6f3362243d6deb
Felipe Lugokinski <128058079+Felipaodoscompiuter@users.noreply.github.com>
2024-05-06 08:37:20 -0300

    Add files via upload

--- CONTAGEM/main.js
+++ CONTAGEM/main.js
@@ -1,7 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores= document.querySelectorAll(".contador");
-const tempoObejetivo1= new Date("2024-12-05T00:00:00");
+const tempoObejetivo1= new Date("2024-11-05T00:00:00");
+const tempoObejetivo2= new Date("2031-01-05T00:00:00");
+const tempoObejetivo3= new Date("2035-01-05T00:00:00");
+const tempoObejetivo4= new Date("2057-03-05T00:00:00");
 const tempoAtual= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -19,5 +22,25 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-contadores[0].textContent = tempoObejetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObejetivo1);
+contadores[1].textContent = calculaTempo(tempoObejetivo2);
+contadores[2].textContent = calculaTempo(tempoObejetivo3);
+contadores[3].textContent = calculaTempo(tempoObejetivo4);
+
+
+function calculaTempo(tempoObejetivo) {
+    let tempoAtual= new Date();
+    let tempoFinal= tempoObejetivo-tempoAtual;
+    let segundos= Math.floor(tempoFinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
+    let anos= Math.floor(dias/365);
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+    dias%=365;
+    return anos+"anos"+dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}
+
 
